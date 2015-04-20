24f4f0b2dabe805a352ed91dc971c885d399c77c
bon22801 <bon22801@gmail.com>
2015-04-20 15:03:42 -0400

    finish them up!

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,3 +1,20 @@
-def stock_picker
+def stock_picker(daily_price)
+  
+  maxProfit = 0
+  buy_day, sell_day = 0
+  
+  for buy_on in 0 ... daily_price.size
+	for sell_on in buy_on ... daily_price.size
+	  if daily_price[sell_on] - daily_price[buy_on] > maxProfit
+		maxProfit = daily_price[sell_on] - daily_price[buy_on]
+        buy_day, sell_day = buy_on, sell_on
+	  end    
+	end
+  end
 
+  if maxProfit == 0
+    puts "don't buy! It's a bad time to invest!"
+  else
+    [buy_day,sell_day]
+  end
 end
--- stock_picker_spec.rb
+++ stock_picker_spec.rb
@@ -1,6 +1,9 @@
 require_relative "stock_picker"
 
 describe "stock_picker" do
-
+  it "returns an array of the best day to buy and sell" do
+    expect(stock_picker([17,3,6,9,15,8,6,1,10])).to eq([1,4])
+    expect(stock_picker([18,9,8,12,6,5,14,3,2])).to eq([5,6])
+  end 
 end
 
